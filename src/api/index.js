248ea5f3a00086224789b383fcb66dca99fd0aa7
SunXa <130429781+sooyaa2000@users.noreply.github.com>
2024-05-03 14:40:39 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -45,7 +45,7 @@ export const getPlayerList = async (server, type, id) => {
 
 // 获取一言数据
 export const getHitokoto = async () => {
-  const res = await fetch("https://v1.hitokoto.cn");
+  const res = await fetch("https://v.api.aa1.cn/api/yiyan/index.php");
   return await res.json();
 };
 
